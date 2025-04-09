05588abcc202b2980fc2888754229a00fa7900c3
Debashis <47478624+debashisda@users.noreply.github.com>
2025-04-09 13:44:20 +0100

    commit

---
+++ Tomcat/bin/setenv.bat
@@ -0,0 +1,16 @@
+@echo off
+
+set JAVA_HOME=C:\Program Files\Java\jdk-17
+set CATALINA_HOME=C:\Users\debas\Documents\Pega Upgrade POC\apache-tomcat
+set "CATALINA_BASE=%CATALINA_HOME%"
+
+set JAVA_OPTS=-DNodeType=BackgroundProcessing,WebUser,Search 
+set JAVA_OPTS=%JAVA_OPTS% -Dname=PEGA-SUPPORT 
+set JAVA_OPTS=%JAVA_OPTS% -Didentification.nodeid=PEGA-SUPPORT 
+set JAVA_OPTS=%JAVA_OPTS% -Djdk.tls.rejectClientInitiatedRenegotiation=true 
+set JAVA_OPTS=%JAVA_OPTS% -Djava.security.egd=file:///dev/urandom 
+set JAVA_OPTS=%JAVA_OPTS% -Djava.awt.headless=true
+
+set JAVA_OPTS=%JAVA_OPTS% "-Dpega.logdir=%CATALINA_HOME%\logs"
+set JAVA_OPTS=%JAVA_OPTS% "-Dpega.tmpdir=%CATALINA_HOME%\temp"
+set JAVA_OPTS=%JAVA_OPTS% "-Dindex.directory=%CATALINA_HOME%\index"
