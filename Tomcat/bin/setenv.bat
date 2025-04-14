55fb0e14faa34aa3fcf9ca773a2445fe8552e8f7
Debashis <47478624+debashisda@users.noreply.github.com>
2025-04-15 00:29:48 +0100

    Update setenv.bat

--- Tomcat/bin/setenv.bat
+++ Tomcat/bin/setenv.bat
@@ -1,10 +1,12 @@
 @echo off
 
 set JAVA_HOME=C:\Program Files\Java\jdk-17
-set CATALINA_HOME=\apache-tomcat
+set CATALINA_HOME=C:\apache-tomcat
 set "CATALINA_BASE=%CATALINA_HOME%"
 
 set JAVA_OPTS=-DNodeType=BackgroundProcessing,WebUser,Search 
+rem set JAVA_OPTS==%JAVA_OPTS% -DNodeTypeStream 
+
 set JAVA_OPTS=%JAVA_OPTS% -Dname=PEGA-SUPPORT 
 set JAVA_OPTS=%JAVA_OPTS% -Didentification.nodeid=PEGA-SUPPORT 
 set JAVA_OPTS=%JAVA_OPTS% -Djdk.tls.rejectClientInitiatedRenegotiation=true 
